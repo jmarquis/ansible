907826963ed928c831b45289708a508ae6b4bda4
Jeremy Marquis <jeremy@jeremymarquis.com>
2014-09-14 22:09:01 -0400

    prospective implementation

--- ansible.js
+++ ansible.js
@@ -1,5 +1,27 @@
-var director = require("director"),
-	router = new director.Router();
+var route = require("path-match")();
+
+var Router = function () {
+
+	this.table = [];
+
+	this.on = function (action, path, fn) {
+		this.table.push({
+			match: route(action + "|" + path),
+			fn: fn
+		});
+	};
+
+	this.match = function (action, path, data) {
+		for (var i = 0; i < this.table.length; i++) {
+			var params = this.table[i].match(action + "|" + path);
+			if (params !== false) return this.table[i].fn(params, data);
+		}
+		return false;
+	};
+
+};
+
+var router = new Router();
 
 module.exports = function (io, debug) {
 
@@ -7,15 +29,36 @@ module.exports = function (io, debug) {
 
 		if (debug) console.log("Incoming connection");
 
-		socket.emit("ansible:update", { some: "data" });
+		var retrieve = function (channel) {
+			var data = router.match("get", channel);
+			if (data) {
+				socket.emit("ansible:update", {
+					channel: channel,
+					data: data
+				});
+			}
+		};
 
-		socket.on("ansible:joinroom", function (room) {
-			if (debug) console.log("Joining room: " + room);
-			socket.join(room);
-			router.dispatch("get", room, function (err, data) {
-				console.log(err);
-				console.log(data);
-			});
+		socket.on("ansible:subscribe", function (channel) {
+			if (debug) console.log("Subscribing: " + channel);
+			socket.join(channel);
+			retrieve(channel);
+		});
+
+		socket.on("ansible:unsubscribe", function (channel) {
+			if (debug) console.log("Unsubscribing: " + channel);
+			socket.leave(channel);
+		});
+
+		socket.on("ansible:get", function (channel) {
+			if (debug) console.log("Retrieving: " + channel);
+			retrieve(channel);
+		});
+
+		socket.on("ansible:update", function (data) {
+			if (debug) console.log("Incoming update: " + data.channel);
+			router.match("update", data.channel);
+			socket.broadcast.emit("ansible:update", data);
 		});
 
 	});
